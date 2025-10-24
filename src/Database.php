64def2b3f332d847934a69021f36db0aad0661c3
Ahmed Adel <ahmedadel@gmail.com>
2025-10-24 06:43:50 +0300

    Fix Database connection and PHPUnit setup

--- src/Database.php
+++ src/Database.php
@@ -3,29 +3,27 @@
 namespace App;
 
 use PDO;
-class Database{
+
+class Database {
     private static ?PDO $pdo = null;
 
     public static function getConnection(): PDO
     {
         if (self::$pdo === null){
-            $host = 'localhost';
-            $dbname ='oop_project';
+            $host = '127.0.0.1'; // استخدم 127.0.0.1 بدل localhost
+            $dbname = 'oop_project';
             $user = 'root';
-            $password = 'root';
-            $opchens = [
+            $password = '';
+            $options = [ // صححت typo
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
             ];
 
             $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-            self::$pdo = new PDO($dsn, $user, $password, $opchens);
+            self::$pdo = new PDO($dsn, $user, $password, $options);
         }
         return self::$pdo;      
-
     }
-
 }
-
 ?>
